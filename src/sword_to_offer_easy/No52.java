de110ec2a4cf001e9276254374ea3eacfc12e107
James Qiu <qiuqiang_keepfaith@163.com>
2021-01-01 17:24:41 +0800

    开始刷题

--- src/sword_to_offer_easy/No52.java
+++ src/sword_to_offer_easy/No52.java
@@ -1,4 +1,91 @@
 package sword_to_offer_easy;
 
-public class No52 {
+import java.util.HashSet;
+
+/*题目：
+* 输入两个链表，找出它们的第一个公共节点
+*/
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
 }
+
+
+
+public class No52 {
+    //解法1：使用set筛重
+    public ListNode getIntersectionNode1(ListNode headA, ListNode headB) {
+        HashSet<ListNode> set = new HashSet<>();
+        ListNode temp = headA;
+        while (temp != null){
+            set.add(temp);
+            temp = temp.next;
+        }
+
+        ListNode temp2 = headB;
+        while (temp2 != null){
+            /*if (set.add(temp2)){  //不应该使用添加的方法，有自己在集合中判断某个元素是否存在的方法
+                temp2 = temp2.next;
+            }else{
+                break;
+            }*/
+            if(set.contains(temp2)){
+                break;
+            }else{
+                temp2 = temp2.next;
+            }
+        }
+        return temp2;
+    }
+
+
+    //解法2：使用双指针
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
+
+        while (temp1 != temp2){
+        //没有必要避开同时为null导致while判断相等的情况，同时为null，说明等长，等长却相等为null，说明没有交点
+            if (temp1 == null){
+                temp1 = headB;
+            }else{
+                temp1 = temp1.next;
+            }
+
+            if (temp2 == null){
+                temp2 = headA;
+            }else{
+                temp2 = temp2.next;
+            }
+        }
+
+        return temp1;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/sword_to_offer_easy/No53.java
@@ -0,0 +1,29 @@
+package sword_to_offer_easy;
+
+/*题目：
+* 统计一个数字在排序(注意，是已排好序的)数组中出现的次数。
+* */
+public class No53 {
+    public int search(int[] nums, int target) {
+        // 搜索右边界 right
+        int i = 0, j = nums.length - 1;
+        while(i <= j) {
+            int m = (i + j) / 2;
+            if(nums[m] <= target) i = m + 1;
+            else j = m - 1;
+        }
+        int right = i;
+        // 若数组中无 target ，则提前返回
+        if(j >= 0 && nums[j] != target) return 0;
+        // 搜索左边界 right
+        i = 0; j = nums.length - 1;
+        while(i <= j) {
+            int m = (i + j) / 2;
+            if(nums[m] < target) i = m + 1;
+            else j = m - 1;
+        }
+        int left = j;
+        return right - left - 1;
+    }
+
+}
